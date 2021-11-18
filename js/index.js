7d183277844f2d877be6c08e62ea6c73e13e541b
Max D <max.moderation@gmail.com>
2021-11-18 21:46:18 +0200

    todo: fix clicks

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
 const cards = document.querySelectorAll('.memory-card');
+// const el = document.querySelector('#user');
+
+let isRotate = false;
+let lockCards = false;
+let firstCard, secondCard;
 
 function cardClicked() {
-    this.classList.toggle('rotate');
+    if (lockCards) return;
+    if (this === firstCard) return;
+
+    this.classList.add('rotate');
+
+    if (!isRotate) {
+        isRotate = true;
+        firstCard = this;
+    } else {
+        isRotate = false;
+        secondCard = this;
+
+        console.log(firstCard.dataset.webimg);
+        console.log(secondCard.dataset.webimg);
+
+        if (firstCard.dataset.webimg === secondCard.dataset.webimg) {
+            // is matching!
+            firstCard.removeEventListener('click', cardClicked);
+            secondCard.removeEventListener('click', cardClicked);
+        } else {
+            // is no mathing
+            setTimeout(() => {
+                firstCard.classList.remove('rotate');
+                secondCard.classList.remove('rotate');
+            }, 1000);
+
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', cardClicked));
