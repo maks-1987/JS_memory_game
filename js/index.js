7f2553cbfeeb607a47289deedfeb292fa482362d
Max D <max.moderation@gmail.com>
2021-11-25 20:38:19 +0200

    add reset button

--- js/index.js
+++ js/index.js
@@ -1,58 +1,77 @@
 const cards = document.querySelectorAll('.memory-card');
+const reset_btn = document.querySelector('.reset-btn');
 
 let isRotate = false;
 let lockCards = false;
 let firstCard, secondCard;
 
 function cardClicked() {
-    if (lockCards) return;
-    if (this === firstCard) return;
+  if (lockCards) return;
+  if (this === firstCard) return;
 
-    this.classList.add('rotate');
+  this.classList.add('rotate');
 
-    if (!isRotate) {
-        // click one
-        isRotate = true;
-        firstCard = this;
+  if (!isRotate) {
+    // click one
+    isRotate = true;
+    firstCard = this;
 
-        return;
-    }
-    // click two
-    secondCard = this;
+    return;
+  }
+  // click two
+  secondCard = this;
 
-    checkMatching();
+  checkMatching();
 }
 
 function checkMatching() {
-    if (firstCard.dataset.webimg === secondCard.dataset.webimg) {
-        // is matching!
-        firstCard.removeEventListener('click', cardClicked);
-        secondCard.removeEventListener('click', cardClicked);
+  if (firstCard.dataset.webimg === secondCard.dataset.webimg) {
+    // is matching!
+    firstCard.removeEventListener('click', cardClicked);
+    secondCard.removeEventListener('click', cardClicked);
 
-        resetCards();
-    } else {
-        // is no mathing
-        lockCards = true;
+    resetCards();
+  } else {
+    // is no mathing
+    lockCards = true;
+    hideCard();
+  }
+}
 
-        setTimeout(() => {
-            firstCard.classList.remove('rotate');
-            secondCard.classList.remove('rotate');
+function hideCard() {
+  setTimeout(() => {
+    firstCard.classList.remove('rotate');
+    secondCard.classList.remove('rotate');
 
-            resetCards();
-        }, 1000);
-    }
+    resetCards();
+  }, 1000);
 }
 
 function resetCards() {
-    [isRotate, lockCards] = [false, false];
-    [firstCard, secondCard] = [null, null];
+  [isRotate, lockCards] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
-(function mixCards() {
-    cards.forEach(card => {
-        let position = Math.floor(Math.random() * 12);
-        card.style.order = position;
-    });
-})()
+mixCards();
 
-cards.forEach(card => card.addEventListener('click', cardClicked));
+function mixCards() {
+  cards.forEach((card) => {
+    let position = Math.floor(Math.random() * 12);
+    card.style.order = position;
+  });
+}
+
+function resetBoards() {
+  mixCards();
+  resetCards();
+
+  cards.forEach((card) => {
+    card.classList.remove('rotate');
+    // card.addEventListener('click', cardClicked);
+  });
+
+  cards.forEach((card) => card.addEventListener('click', cardClicked));
+}
+
+cards.forEach((card) => card.addEventListener('click', cardClicked));
+reset_btn.addEventListener('click', resetBoards);
